b25bcbc0b931ddafad73e428649e326da875d66c
hangyu cho <40537312+hangyuCho@users.noreply.github.com>
2021-10-22 22:59:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // TODO: Title here
 // Body here
 console.log("hello world!");
-// TODO: todo test
-// body hogehoge
+
+// TODO: Add integration test for TodoActionsMain.
+//
+// Code that interface with external data have been separated into their own modules.
+// These includes:
+//
+// - `DataStore`
+// - `CodeRepository`
+// - `TaskManagementSystem`
+//
+// They can be mocked by creating a mock version using `__mocks__` folder.
+// https://jestjs.io/docs/en/manual-mocks
 console.log("hoge!!!!!!!!!!!");
