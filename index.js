8d6549bbfbeb8215ab6a505c374e10e0befb61de
hangyu cho <40537312+hangyuCho@users.noreply.github.com>
2021-10-22 23:24:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-// TODO [#1]: implement this thing
+// TODO Come up with a more imaginative greeting
+//  Everyone uses hello world and it's boring.
+// user projects: alstr/Test User Project/To Do
+//  org projects: alstrorg/Test Org Project/To Do
 console.log("hoge33333");
